f6dc9425664f26b63aa54a6116cc6fae1cdb4121
amaranggana-sch <111202113688@mhs.dinus.ac.id>
2023-03-22 00:39:58 +0700

    added python code and data
    lorem ipsum

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,14 @@
+from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv(
+    r'E:\Documents\College\VS Code\git\Tugas-Pertemuan-ke-3-Preprocessing-Data\Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, -1].values
 print(X)
 print(Y)
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=0)
